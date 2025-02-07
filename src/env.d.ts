3b6cd54bd9b0ffc8a5315903016e71393b5bdeaa
Jake Kinchen <jake.kinchen@onetier.com>
2025-02-06 23:10:18 -0600

    Add type declarations for Markdown and MDX files

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module '*.mdx' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.md' {
+  const content: any;
+  export default content;
+}
